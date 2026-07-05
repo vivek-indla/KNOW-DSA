ea19122b78a3599b735494dc016affcf48121ae7
vivek indla <25215a7213@bvrit.ac.in>
2026-07-05 17:57:29 +0530

    Time: 0 ms (100%), Space: 10.5 MB (30.34%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        string vowels;
-        unordered_set<char> set1={'a','e','i','o','u','A','E','I','O','U'};
-        int left=0;
-        int right=s.length()-1;
-        while(left<=right){
-            if(set1.find(s[left])!=set1.end() && set1.find(s[right])!=set1.end()){
-                    swap(s[left],s[right]);
-                    left++;
-                    right--;
+        // unordered_set<char> set1={'a','e','i','o','u','A','E','I','O','U'};
+        // int left=0;
+        // int right=s.length()-1;
+        // while(left<=right){
+        //     if(set1.find(s[left])!=set1.end() && set1.find(s[right])!=set1.end()){
+        //             swap(s[left],s[right]);
+        //             left++;
+        //             right--;
+        //     }
+        //     else if(set1.find(s[left])==set1.end()){
+        //         left++;
+        //     }
+        //     else{
+        //         right--;
+        //     }
+        // }
+        // return s; 
+        // above is my approach with 4ms let's learn more optimal approach
+        string word = s;
+        int start = 0;
+        int end = s.length() - 1;
+        string vowels = "aeiouAEIOU";
+        while (start < end) {
+            while (start < end && vowels.find(word[start]) == string::npos) {
+                start++;
             }
-            else if(set1.find(s[left])==set1.end()){
-                left++;
+            while (start < end && vowels.find(word[end]) == string::npos) {
+                end--;
             }
-            else{
-                right--;
-            }
+            swap(word[start], word[end]);
+            start++;
+            end--;
         }
-        return s;
+        return word;
     }
 };
