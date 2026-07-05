3fec5d818cae88e2ae016b9a0e7a0e746096b3e4
vivek indla <25215a7213@bvrit.ac.in>
2026-07-05 09:17:12 +0530

    Time: 0 ms (100%), Space: 7.9 MB (11.98%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0|| n>pow(2,31)-1||n<0.0000000004656612873077392578125){
-            return false;
-        }
-        int val=log2(abs(n));
-        if(pow(2,val)==n){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return n>0 && (n&(n-1))==0;
     }
 };
