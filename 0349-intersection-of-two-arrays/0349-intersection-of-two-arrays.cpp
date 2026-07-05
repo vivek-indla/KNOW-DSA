585c3496fc2f00d364393e79f2165b89bffde03d
vivek indla <25215a7213@bvrit.ac.in>
2026-07-05 11:18:41 +0530

    Time: 0 ms (100%), Space: 14.5 MB (27.98%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<int> set1(nums1.begin(),nums1.end());
         unordered_set<int> set2(nums2.begin(),nums2.end());
         vector<int> result;
-        for(auto &i:set1){
+        for(const auto& i:set1){
             if(set2.find(i)!=set2.end()){
                 result.push_back(i);
             }
