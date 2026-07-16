c491276a28d1fc6583e056ea7f381fa3827f7aef
vivek indla <25215a7213@bvrit.ac.in>
2026-07-16 21:47:32 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // int n=nums.size();
-        // int left=0;
-        // int mid=0;
-        // int right=n-1;
-        // while(mid<=right){
-        //     if(nums[mid]==0){
-        //         swap(nums[left],nums[mid]);
-        //         left++;
-        //         mid++;
-        //     }
-        //     else if(nums[mid]==1){
-        //         mid++;
-        //     }
-        //     else{
-        //         swap(nums[mid],nums[right]);
-        //         right--;
-        //     }
-        // }
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int left=0;
+        int mid=0;
+        int right=n-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left],nums[mid]);
+                left++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[right]);
+                right--;
+            }
+        }
+       // sort(nums.begin(),nums.end());
     }
 };
