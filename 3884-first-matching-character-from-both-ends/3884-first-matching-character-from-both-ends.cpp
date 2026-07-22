7c942bd10a0ce98b4254736e0ffce98261e6158f
vivek indla <25215a7213@bvrit.ac.in>
2026-07-22 16:54:57 +0530

    Time: 0 ms (100%), Space: 9 MB (28.2%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int firstMatchingIndex(string s) {
-        int n=s.length();
         int left=0;
-        int right=n-1;
+        int right=s.length()-1;
         while(left<=right){
             if(s[left]==s[right]){
                 return left;
