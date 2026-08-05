a311b08a278503f321b8ee184f6ec43d158bc824
vivek indla <25215a7213@bvrit.ac.in>
2026-08-05 17:41:40 +0530

    Time: 12 ms (10.22%), Space: 124.4 MB (32.82%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,23 +8,55 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ ListNode* reverseLL(ListNode* head){
+    if(head==NULL||head->next==NULL) return head;
+    ListNode* newHead=reverseLL(head->next);
+    ListNode* front=head->next;
+    front->next=head;
+    head->next=NULL;
+    return newHead;
+ }
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp=head;
-        while(temp){
-            st.push(temp->val);
-            temp=temp->next;
+        // stack<int> st;
+        // ListNode* temp=head;
+        // while(temp){
+        //     st.push(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // while(temp){
+        //     if(temp->val!=st.top()){
+        //         return false;
+        //     }
+        //     st.pop();
+        //     temp=temp->next;
+        // }
+        // return true;
+        // above is the brute force approach time: O(2n) space: O(N) for using external space stack
+
+        if(head==NULL || head->next==NULL){
+            return true;
         }
-        temp=head;
-        while(temp){
-            if(temp->val!=st.top()){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newHead=reverseLL(slow->next);
+        ListNode* first=head;
+        ListNode* second=newHead;
+        while(second!=NULL){
+            if(first->val!=second->val){
+                reverseLL(newHead);
                 return false;
             }
-            st.pop();
-            temp=temp->next;
+            first=first->next;
+            second=second->next;
         }
+        reverseLL(newHead);
         return true;
     }
 };
