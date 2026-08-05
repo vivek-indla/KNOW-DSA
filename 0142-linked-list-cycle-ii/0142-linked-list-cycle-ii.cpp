86e8a57b3cc9731511c2ad8a94e6c0d96e33e7d6
vivek indla <25215a7213@bvrit.ac.in>
2026-08-05 23:53:18 +0530

    Time: 10 ms (45.36%), Space: 11.4 MB (24.57%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,14 +9,31 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* temp=head;
-        map<ListNode*,int> mapp;
-        while(temp){
-            if(mapp.find(temp)!=mapp.end()){
-                return temp;
+        // ListNode* temp=head;
+        // map<ListNode*,int> mapp;
+        // while(temp){
+        //     if(mapp.find(temp)!=mapp.end()){
+        //         return temp;
+        //     }
+        //     mapp[temp]=1;
+        //     temp=temp->next;
+        // }
+        // return nullptr;
+        // above is a brute force approach time around:O(N) space:O(N)
+
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
             }
-            mapp[temp]=1;
-            temp=temp->next;
         }
         return nullptr;
     }
