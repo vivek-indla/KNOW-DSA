341460fb5741fdc2d09ebe1bc6b17cde44f02f24
vivek indla <25215a7213@bvrit.ac.in>
2026-08-06 16:26:09 +0530

    Time: 0 ms (100%), Space: 312.2 MB (16.55%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -14,24 +14,38 @@ public:
         if(head==NULL || head->next==NULL){
             return NULL;
         }
-        ListNode* temp=head;
-        int count=0;
-        while(temp){
-            count++;
-            temp=temp->next;
-        }  
-        int pos=count/2;
-        temp=head;
-        while(temp){
-            pos--;
-            if(pos==0){
-                ListNode* delNode=temp->next;
-                temp->next=delNode->next;
-                delete delNode;
-                break;
-            }
-            temp=temp->next;
-        }
+        // ListNode* temp=head;
+        // int count=0;
+        // while(temp){
+        //     count++;
+        //     temp=temp->next;
+        // }  
+        // int pos=count/2;
+        // temp=head;
+        // while(temp){
+        //     pos--;
+        //     if(pos==0){
+        //         ListNode* delNode=temp->next;
+        //         temp->next=delNode->next;
+        //         delete delNode;
+        //         break;
+        //     }
+        //     temp=temp->next;
+        // }
+        // return head;
+        // above is brute approach time: O(N + N/2) space:O(1)
+
+        // we can optimize this to time:O(N/2) space:O(1)
+        ListNode* slow=head;
+        ListNode* fast=head;
+        fast=head->next->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+    }
+        ListNode* delNode=slow->next;
+        slow->next=slow->next->next;
+        delete delNode;
         return head;
     }
 };
