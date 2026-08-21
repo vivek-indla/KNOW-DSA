f6e8685b683a150eb3d5830f526f3dbbe228708e
vivek indla <25215a7213@bvrit.ac.in>
2026-08-21 10:17:07 +0530

    Time: 103 ms (9.74%), Space: 18.4 MB (82.72%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -20,19 +20,51 @@
     }
     return head;
  }
+ ListNode* mergeTwoLists(ListNode* list1,ListNode* list2){
+    ListNode* dummyHead=new ListNode(-1);
+    ListNode* traverse=dummyHead;
+    while(list1 && list2){
+        if(list1->val < list2->val){
+            traverse->next=list1;
+            list1=list1->next;
+        }
+        else{
+            traverse->next=list2;
+            list2=list2->next;
+        }
+        traverse=traverse->next;
+    }
+    if(list1){
+        traverse->next=list1;
+    }
+    else{
+        traverse->next=list2;
+    }
+    return dummyHead->next;
+ }
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> arr;
-        for(int i=0;i<lists.size();i++){
-            ListNode* temp=lists[i];
-            while(temp){
-                arr.push_back(temp->val);
-                temp=temp->next;
-            }
+        // vector<int> arr;
+        // for(int i=0;i<lists.size();i++){
+        //     ListNode* temp=lists[i];
+        //     while(temp){
+        //         arr.push_back(temp->val);
+        //         temp=temp->next;
+        //     }
+        // }
+        // sort(arr.begin(),arr.end());
+        // ListNode* head=convert(arr);
+        // return head;
+        // above is the brute approach use time : O(N Log N) space : O(N)
+        
+        //second approach
+        if(lists.empty())
+            return NULL;
+        ListNode* head=lists[0];
+        for(int i=1;i<lists.size();i++){
+            head=mergeTwoLists(head,lists[i]);
         }
-        sort(arr.begin(),arr.end());
-        ListNode* head=convert(arr);
         return head;
     }
 };
