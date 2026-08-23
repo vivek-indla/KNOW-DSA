de8cdec1a1e42a68704af90dd9e2c90a0e862b45
vivek indla <25215a7213@bvrit.ac.in>
2026-08-23 23:25:56 +0530

    Time: 2 ms (20.16%), Space: 15.7 MB (7.55%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> leftSum;
         vector<int> rightSum;
         int sum=0;
+        int n=nums.size()-1;
         for(int i=0;i<nums.size();i++){
             leftSum.push_back(sum);
             sum+=nums[i];
@@ -13,10 +14,10 @@ public:
             rightSum.push_back(rsum);
             rsum+=nums[i];
         }
-        reverse(rightSum.begin(),rightSum.end());
+        // reverse(rightSum.begin(),rightSum.end());
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            ans.push_back(abs(leftSum[i]-rightSum[i]));
+            ans.push_back(abs(leftSum[i]-rightSum[n-i]));
         }
         return ans;
     }
