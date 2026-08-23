77bdd1a27ce5af18a69cab01c1bf559451beeb39
vivek indla <25215a7213@bvrit.ac.in>
2026-08-23 08:47:26 +0530

    Time: 0 ms (100%), Space: 11 MB (86.88%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -8,43 +8,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- ListNode* reverseList(ListNode* head){
-    ListNode* curr=head;
-    ListNode* prev=NULL;
-    while(curr){
-        ListNode* temp=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=temp;
-    }
-    return prev;
-    }
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
         if(head==NULL || head->next==NULL){
             return head;
         }
-        ListNode* newHead;
-        ListNode* temp=head;
-        ListNode* prevNode=NULL;
-        while(temp!=NULL){
-            ListNode* next=temp->next;
-            if(next==NULL){
-                if(prevNode)
-                    prevNode->next=temp;
-                break;
-            }
-            ListNode* nextNode=next->next;
-            next->next=NULL;
-            reverseList(temp);
-            if(prevNode==NULL)
-                newHead=next;
-            else
-                prevNode->next=next;
-            prevNode=temp;
-            temp=nextNode;
-        }
-        return newHead;
+        ListNode* temp=head->next;
+        head->next=swapPairs(temp->next);
+        temp->next=head;
+        return temp;
     }
 };
