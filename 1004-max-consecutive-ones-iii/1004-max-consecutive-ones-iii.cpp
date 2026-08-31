f891661fb55be5567f3e2ae4c1488356f489cd8b
vivek indla <25215a7213@bvrit.ac.in>
2026-08-31 23:17:52 +0530

    Time: 0 ms (100%), Space: 69.6 MB (69.85%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,5 +1,20 @@
 class Solution {
 public:
+    // brute force approrch time: O(2n) and space: O(1)    
+    // int longestOnes(vector<int>& nums, int k) {
+    //     int left=0,right=0,maxlen=0,zeros=0;
+    //     while(right<nums.size()){
+    //         if(nums[right]==0) zeros++;
+    //         if(zeros > k){
+    //             if(nums[left]==0) zeros--;
+    //             left++;
+    //         }
+    //         maxlen=max(maxlen,right-left+1);
+    //         right++;
+    //     }
+    //     return maxlen;
+    // }
+    //optimal approach time: O(N) and space: O(1)
     int longestOnes(vector<int>& nums, int k) {
         int left=0,right=0,maxlen=0,zeros=0;
         while(right<nums.size()){
