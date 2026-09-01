8e76e52afa0ecdc84580e201274766a06242ae2d
vivek indla <25215a7213@bvrit.ac.in>
2026-09-01 06:54:26 +0530

    Time: 52 ms (23.05%), Space: 88.8 MB (7.46%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        // int left=0,right=0,maxlen=0;
+        // map<int,int> hashh;
+        // while(right<fruits.size()){
+        //     hashh[fruits[right]]++;
+        //     while(hashh.size()>2){
+        //         hashh[fruits[left]]--;
+        //         if(hashh[fruits[left]]==0) 
+        //             hashh.erase(fruits[left]);
+        //         left++;
+        //     }
+        //     maxlen=max(maxlen,right-left+1);
+        //     right++;
+        // }
+        // return maxlen;
+        // better approach time: O(2N) and space:O(3)
+
+        //optimal approach time: O(N) and space:O(3)
         int left=0,right=0,maxlen=0;
         map<int,int> hashh;
         while(right<fruits.size()){
             hashh[fruits[right]]++;
-            while(hashh.size()>2){
+            if(hashh.size()>2){
                 hashh[fruits[left]]--;
                 if(hashh[fruits[left]]==0) 
                     hashh.erase(fruits[left]);
