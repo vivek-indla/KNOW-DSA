49034b0269f40f3479e47e57fb8fb0b810926863
vivek indla <25215a7213@bvrit.ac.in>
2026-09-01 13:11:52 +0530

    Time: 1 ms (87.34%), Space: 161.2 MB (92.19%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -22,36 +22,36 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        // head=reverseList(head);
-        // ListNode* temp=head;
-        // int val=temp->val;
-        // while(temp && temp->next){
-        //     ListNode* nextNode=temp->next;
-        //     while(nextNode && val>nextNode->val){
-        //         nextNode=nextNode->next;
-        //     }
-        //     if(nextNode) val=nextNode->val;
-        //     temp->next=nextNode;
-        //     temp=temp->next;
-        // }
-        // head=reverseList(head);
-        // return head;
-        //above is my approach time : O(4N) and space :O(1)
-
-        //optimal approach
-        vector<ListNode*> st;
+        head=reverseList(head);
         ListNode* temp=head;
-        while(temp){
-        while(!st.empty() && st.back()->val < temp->val){
-            st.pop_back();
+        int val=temp->val;
+        while(temp && temp->next){
+            ListNode* nextNode=temp->next;
+            while(nextNode && val>nextNode->val){
+                nextNode=nextNode->next;
+            }
+            if(nextNode) val=nextNode->val;
+            temp->next=nextNode;
+            temp=temp->next;
         }
-        st.push_back(temp);
-        temp=temp->next;
-        }
-        for(int i=0;i+1<st.size();i++){
-            st[i]->next=st[i+1];
-        }
-        st.back()->next=nullptr;
-        return st[0];
+        head=reverseList(head);
+        return head;
+        // above is my approach time : O(4N) and space :O(1)
+
+        //optimal approach time: O(N) and space : O(N) for vector storage
+        // vector<ListNode*> st;
+        // ListNode* temp=head;
+        // while(temp){
+        // while(!st.empty() && st.back()->val < temp->val){
+        //     st.pop_back();
+        // }
+        // st.push_back(temp);
+        // temp=temp->next;
+        // }
+        // for(int i=0;i+1<st.size();i++){
+        //     st[i]->next=st[i+1];
+        // }
+        // st.back()->next=nullptr;
+        // return st[0];
     }
 };
