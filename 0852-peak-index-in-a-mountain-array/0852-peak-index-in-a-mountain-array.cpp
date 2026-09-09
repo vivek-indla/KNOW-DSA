c05171d12fe08494325ee13e62dc021c561e6d0e
vivek indla <25215a7213@bvrit.ac.in>
2026-09-09 16:35:12 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.81%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int left=0;
+        int left=1;
         int right=arr.size()-1;
         while(left<=right){
             int mid=left+(right-left)/2;
@@ -9,7 +9,7 @@ public:
                 return mid;
             }
             if(arr[mid]<arr[mid-1]){
-                right=mid;
+                right=mid-1;
             }
             else{
                 left=mid+1;
