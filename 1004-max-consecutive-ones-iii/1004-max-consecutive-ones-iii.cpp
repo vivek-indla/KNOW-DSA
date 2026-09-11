4ec10bd9bdcd6d3151394e18bc72241fc7cedfb5
vivek indla <25215a7213@bvrit.ac.in>
2026-09-11 15:08:45 +0530

    Time: 0 ms (100%), Space: 69.5 MB (70.53%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    // brute force approrch time: O(2n) and space: O(1)    
-    // int longestOnes(vector<int>& nums, int k) {
-    //     int left=0,right=0,maxlen=0,zeros=0;
-    //     while(right<nums.size()){
-    //         if(nums[right]==0) zeros++;
-    //         if(zeros > k){
-    //             if(nums[left]==0) zeros--;
-    //             left++;
-    //         }
-    //         maxlen=max(maxlen,right-left+1);
-    //         right++;
-    //     }
-    //     return maxlen;
-    // }
-    //optimal approach time: O(N) and space: O(1)
     int longestOnes(vector<int>& nums, int k) {
-        int left=0,right=0,maxlen=0,zeros=0;
-        while(right<nums.size()){
+         //better approach time: O(2n) and space:O(n)
+        // int right=0,left=0,maxlen=0,zeros=0;
+        // int n=nums.size()-1;
+        // while(right<n){
+        //     if(nums[right]==0) zeros++;
+        //     while(zeros > k){
+        //         if(nums[left]==0) zeros--;
+        //         left++;
+        //     }
+        //     maxlen=max(maxlen,right-left+1);
+        //     right++;
+        // }
+        // return maxlen;
+
+         //optimal approach time:O(N) and space:O(1)
+        int right=0,left=0,maxlen=0,zeros=0;
+        int n=nums.size()-1;
+        while(right<=n){
             if(nums[right]==0) zeros++;
             if(zeros > k){
                 if(nums[left]==0) zeros--;
