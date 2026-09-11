8c050cfc027f26d68624773d1a9d9825f6f15086
vivek indla <25215a7213@bvrit.ac.in>
2026-09-11 15:23:53 +0530

    Time: 65 ms (5.01%), Space: 88.9 MB (7.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        // int left=0,right=0,maxlen=0;
-        // map<int,int> hashh;
-        // while(right<fruits.size()){
-        //     hashh[fruits[right]]++;
-        //     while(hashh.size()>2){
-        //         hashh[fruits[left]]--;
-        //         if(hashh[fruits[left]]==0) 
-        //             hashh.erase(fruits[left]);
-        //         left++;
-        //     }
-        //     maxlen=max(maxlen,right-left+1);
-        //     right++;
-        // }
-        // return maxlen;
-        // better approach time: O(2N) and space:O(3)
-
-        //optimal approach time: O(N) and space:O(3)
-        int left=0,right=0,maxlen=0;
+    int totalFruit(vector<int>& nums) {
+        int right=0,left=0,maxlen=0;
         map<int,int> hashh;
-        while(right<fruits.size()){
-            hashh[fruits[right]]++;
+        while(right<nums.size()){
+            hashh[nums[right]]++;
             if(hashh.size()>2){
-                hashh[fruits[left]]--;
-                if(hashh[fruits[left]]==0) 
-                    hashh.erase(fruits[left]);
-                left++;
+            hashh[nums[left]]--;
+            if(hashh[nums[left]]==0)
+                hashh.erase(nums[left]);    
+            left++;
             }
             maxlen=max(maxlen,right-left+1);
             right++;
