82c44147b37884b074a2921a784c15d302901077
vivek indla <25215a7213@bvrit.ac.in>
2026-09-11 16:02:47 +0530

    Time: 0 ms (100%), Space: 17.2 MB (16.25%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans;
-        int k=2;
-        while(k--){
-            for(int i:nums){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+        nums.insert(nums.end(),nums.begin(),nums.end());
+        return nums;
     }
 };
