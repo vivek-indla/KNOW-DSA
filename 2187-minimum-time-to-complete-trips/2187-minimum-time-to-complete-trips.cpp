a147550a1fd466f1e3e642b40b5ac3a162d0560b
vivek indla <25215a7213@bvrit.ac.in>
2026-09-21 11:57:45 +0530

    Time: 64 ms (36.86%), Space: 98.4 MB (52.25%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        int n=time.size();
+        // if(n==1) return totalTrips;
+        long long mn=*min_element(time.begin(),time.end());
+        long long low=1;
+        long long high=mn*totalTrips;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            long long total=0;
+            for(int i=0;i<n;i++){
+                if(time[i]<=mid){
+                    total+=mid/time[i];
+                }
+            }
+            cout<<mid<<" "<<total<<endl;
+            if(total<totalTrips){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+};
